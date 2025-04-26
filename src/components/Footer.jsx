a7bedc4f27e3ddd21808eb6967408e7847652780
0biin0 <infinite402@gmail.com>
2025-04-26 13:47:23 +0900

    내용 수정

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="footer-fixed">
       <p className="footer-slogan">기록은 기억을 지키는 가장 부드러운 방법입니다.</p>
-      <p className="footer-info">© 2025 DreamLog — Designed & Developed by 김효빈</p>
+      <p className="footer-info">© 2025 DreamLog — Designed by 김효빈 | Developed by 김효빈, 장광진, 박혜연</p>
     </footer>
   );
 }
